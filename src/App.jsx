50a031e157f4236f92fbcfbe40d406d46317edf3
ziaraheem <90146416+ziaraheem@users.noreply.github.com>
2022-05-27 20:32:27 +0530

    new change to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
+import React from "react";
 import "./App.css";
+import MyFuncComp from "./Components/MyFuncComp";
 
-function App() {
-  return <div>I am App Component</div>;
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+    this.update = this.update.bind(this);
+
+    this.state = { date: new Date() };
+  }
+  update() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+  componentDidMount() {
+    console.log("componentDidMount");
+    // setInterval(() => {
+    // function update() {
+    //   this.setState({
+    //     date: new Date(),
+    //   });
+    // }
+    //  }, 1000);
+  }
+
+  render() {
+    console.log("render");
+    return (
+      <div className="header">
+        <i>{this.props.name}I am App Component</i> <br />
+        <b>I am App Component</b>
+        <b>Hey Check Date ==: {this.state.date.toLocaleTimeString()}</b>
+        <button onClick={this.update}>update time</button>
+        <MyFuncComp date={this.state.date.toLocaleTimeString()} />
+      </div>
+    );
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
 }
-
-export default App;
